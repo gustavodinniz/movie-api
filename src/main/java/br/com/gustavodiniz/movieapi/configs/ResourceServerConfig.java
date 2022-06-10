3c2459a96187fdb33efd62df3443c8647c0951d4
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-06-10 19:55:24 -0300

    h2 database security configuration

--- src/main/java/br/com/gustavodiniz/movieapi/configs/ResourceServerConfig.java
+++ src/main/java/br/com/gustavodiniz/movieapi/configs/ResourceServerConfig.java
@@ -1,21 +1,35 @@
 package br.com.gustavodiniz.movieapi.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    @Autowired
+    private Environment env;
+
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**", "/users"};
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        // H2 config
+        if (Arrays.asList(env.getActiveProfiles()).contains("local")) {
+            http.headers().frameOptions().disable();
+        }
+
         http
                 .authorizeRequests()
-                .antMatchers("/users").permitAll()
-                .antMatchers(
-                        "/movies/**").authenticated()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers("/movies/**").authenticated()
                 .anyRequest().denyAll();
 
         ;
